b499300fc69f75ea68771471977b7a38d88185ae
kohlrabi <kohlrabi@aragonit>
2015-02-03 18:46:55 +0100

    ein paar kleinigkeiten

--- Network/Circus.hs
+++ Network/Circus.hs
@@ -1,3 +1,7 @@
-module Network.Circus (module Network.Circus.Session) where
+module Network.Circus (
+    module Network.Circus.Session
+  , module Network.Circus.Events
+  ) where
 
 import Network.Circus.Session
+import Network.Circus.Events
--- Network/Circus/Events.hs
+++ Network/Circus/Events.hs
@@ -9,21 +9,23 @@ data Event = Event
   , eData :: EventData
   }
 
-data EventType = Privmsg
-               | Ping
-               | Join
-               | Mode
-               | Topic
-               | Kick
-               | Quit
-               | Nick
-               | Notice
-               | Disconnect
-               | Part
-               | Numeric
+data EventType =
+    Privmsg
+  | Ping
+  | Join
+  | Mode
+  | Topic
+  | Kick
+  | Quit
+  | Nick
+  | Notice
+  | Disconnect
+  | Part
+  | Numeric
+  deriving (Eq, Show)
 
 data EventData = EventData
   { eUser    :: String
   , eCommand :: String
   , eArgs    :: [String]
-  }
+  } deriving (Eq, Show)
