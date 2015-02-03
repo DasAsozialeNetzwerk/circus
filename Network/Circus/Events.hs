72b2eea312e5b2a931e9a813ecb52db3221d3065
lukasepple <git@lukasepple.de>
2015-02-03 18:24:57 +0100

    simple Event data structures

--- Network/Circus/Events.hs
+++ Network/Circus/Events.hs
@@ -1,1 +1,29 @@
-module Network.Circus.Events () where
+module Network.Circus.Events (
+    Event(..)
+  , EventType(..)
+  , EventData(..)
+  ) where
+
+data Event = Event
+  { eType :: EventType
+  , eData :: EventData
+  }
+
+data EventType = Privmsg
+               | Ping
+               | Join
+               | Mode
+               | Topic
+               | Kick
+               | Quit
+               | Nick
+               | Notice
+               | Disconnect
+               | Part
+               | Numeric
+
+data EventData = EventData
+  { eUser    :: String
+  , eCommand :: String
+  , eArgs    :: [String]
+  }
