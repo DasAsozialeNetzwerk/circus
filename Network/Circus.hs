86fb739fa33ed569ef5fce1bd6c4542371c73b86
lukasepple <git@lukasepple.de>
2015-02-06 14:18:49 +0100

    export the IRCMessage module

--- Network/Circus.hs
+++ Network/Circus.hs
@@ -1,7 +1,9 @@
 module Network.Circus (
     module Network.Circus.Session
   , module Network.Circus.Events
+  , module Network.Circus.IRCMessage
   ) where
 
 import Network.Circus.Session
 import Network.Circus.Events
+import Network.Circus.IRCMessage
